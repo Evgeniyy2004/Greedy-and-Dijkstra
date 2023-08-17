8fdd74855ffd6e954679200fd8a45bca17655fd8
Evgeniyy2004 <evgeniy.ziganshin.04@mail.ru>
2023-08-17 18:25:27 +0500

    DIjkstra is done

--- DijkstraPathFinder.cs
+++ DijkstraPathFinder.cs
@@ -7,98 +7,63 @@ namespace Greedy;
 
 public class DijkstraPathFinder
 {
-	public IEnumerable<PathWithCost> GetPathsByDijkstra(State state, Point start,IEnumerable<Point> targets)
-	{
+    public IEnumerable<PathWithCost> GetPathsByDijkstra(State state, Point start, IEnumerable<Point> targets)
+    {
         Dictionary<Point, DijkstraData> track = new Dictionary<Point, DijkstraData>();
         HashSet<Point> notvisited = new HashSet<Point>();
-        track[start] = new DijkstraData { metka = 0, Previous = null, length=0};
-        
-        //var goalcopy = targets.ToHashSet();
-        int find = 0;
-        List<PathWithCost> all= new List<PathWithCost>();
-        foreach (var e in targets)
+        track[start] = new DijkstraData { Mark = 0, Previous = null, Length = 0 };
+        notvisited.Add(start);
+        while (true)
         {
-            if(find==0)
+            Point? toOpen = null;
+            double topPrice = double.PositiveInfinity;
+            foreach (var node in notvisited)
             {
-                for (int i = 0; i < state.CellCost.GetLength(0); i++)
+                if (track.ContainsKey(node) && track[node].Mark <= topPrice)
                 {
-                    for (int j = 0; j < state.CellCost.GetLength(1); j++) notvisited.Add(new Point(i, j));
+                    toOpen = node;
+                    topPrice = track[node].Mark;
                 }
-                find++;
             }
-            if(!notvisited.Contains(e))
+
+            if (toOpen == null) yield break;
+
+            if (state.Chests.Contains(toOpen.Value))
             {
-                Point[] points = new Point[track[e].length+1];
-                points[track[e].length] = e;
-                var result = track[e].metka;
-                var now = track[e].Previous;
+                Point[] points = new Point[track[toOpen.Value].Length + 1];
+                points[track[toOpen.Value].Length] = toOpen.Value;
+                var result = track[toOpen.Value].Mark;
+                var now = track[toOpen.Value].Previous;
                 while (now != null)
                 {
-                    points[track[now.Value].length]=now.Value;
+                    points[track[now.Value].Length] = now.Value;
                     now = track[now.Value].Previous;
                 }
-                /*if(result<=state.InitialEnergy)*/ all.Add(new PathWithCost(result, points));
-                continue;
+                yield return new PathWithCost(result, points);
             }
 
-            else while (true)
+            for (int i = -1; i <= 1; i++)
             {
-                Point? toOpen = null;
-                double topPrice = double.PositiveInfinity;
-                foreach (var node in notvisited)
+                for (int j = -1; j <= 1; j++)
                 {
-                    if (track.ContainsKey(node) && track[node].metka < topPrice)
-                    {
-                        toOpen = node;
-                        topPrice = track[node].metka;
-                    }
+                    if ((j == 0 && i == 0) || (j != 0 && i != 0)) continue;
+                    if (!state.InsideMap(new Point(toOpen.Value.X + i, toOpen.Value.Y + j))) continue;
+                    if (track[toOpen.Value].Previous != null && track[toOpen.Value].Previous.Value == new Point(toOpen.Value.X + i, toOpen.Value.Y + j)) continue;
+                    if (state.IsWallAt(new Point(toOpen.Value.X + i, toOpen.Value.Y + j))) continue;
+                    var currprice = (int)topPrice + state.CellCost[toOpen.Value.X + i, toOpen.Value.Y + j];
+                    if (!notvisited.Contains(new Point(toOpen.Value.X + i, toOpen.Value.Y + j)) && !track.ContainsKey(new Point(toOpen.Value.X + i, toOpen.Value.Y + j))) notvisited.Add(new Point(toOpen.Value.X + i, toOpen.Value.Y + j));
+                    if (!track.ContainsKey(new Point(toOpen.Value.X + i, toOpen.Value.Y + j)) || track[new Point(toOpen.Value.X + i, toOpen.Value.Y + j)].Mark > currprice) track[new Point(toOpen.Value.X + i, toOpen.Value.Y + j)] = new DijkstraData { Mark = currprice, Previous = toOpen.Value, Length = track[toOpen.Value].Length + 1 };
                 }
-
-                if (toOpen == null) yield break;
-
-                if (toOpen==e)
-                {
-                        Point[] points = new Point[track[e].length+1];
-                        points[track[e].length] = e;
-                        var result = track[e].metka;
-                        var now = track[e].Previous;
-                        while (now != null)
-                        {
-                            points[track[now.Value].length] = now.Value;
-                            now = track[now.Value].Previous;
-                        }
-                        /*if (result <= state.InitialEnergy)*/ all.Add(new PathWithCost(result, points));
-                        break;
-                    }
-
-                for (int i = -1; i <= 1; i++)
-                {
-                    for (int j = -1; j <= 1; j++)
-                    {
-                        if ((j == 0 && i == 0) || (j != 0 && i != 0)) continue;
-                        if (!state.InsideMap(new Point(toOpen.Value.X + i, toOpen.Value.Y + j))) continue;
-                        if (track[toOpen.Value].Previous != null && track[toOpen.Value].Previous.Value == new Point(toOpen.Value.X + i, toOpen.Value.Y + j)) continue;
-                        if (state.IsWallAt(new Point(toOpen.Value.X + i, toOpen.Value.Y + j))) continue;
-                        var currprice = (int)topPrice + state.CellCost[toOpen.Value.X + i, toOpen.Value.Y + j];
-                        if (!track.ContainsKey(new Point(toOpen.Value.X + i, toOpen.Value.Y + j)) || track[new Point(toOpen.Value.X + i, toOpen.Value.Y + j)].metka > currprice) track[new Point(toOpen.Value.X + i, toOpen.Value.Y + j)] = new DijkstraData { metka = currprice, Previous = toOpen.Value, length = track[toOpen.Value].length+1 };
-                    }
-                }
-
-                notvisited.Remove(toOpen.Value);
             }
-        }
-        //Console.WriteLine(find);
-        //Console.WriteLine(targets.Count());
-        var c = all.OrderBy(x => x.Cost);
-        foreach(var e in c) yield return e;
-    }
 
-	
+            notvisited.Remove(toOpen.Value);
+        }
+    }
 }
 
 public class DijkstraData
 {
-	public int metka;
-	public Point? Previous;
-    public int length;
+    public int Mark;
+    public Point? Previous;
+    public int Length;
 }
